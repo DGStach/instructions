d2453f9ca0533a99e5795baf4130e49d79ed9fe9
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-12 15:44:22 +0200

    display pdf file from backend

--- src/Pdf.js
+++ src/Pdf.js
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
-import { Document, Page,pdfjs } from 'react-pdf';
-
-
-let url =
-    "https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK"
-
-
+import React, {useState} from 'react';
+import {Document, Page, pdfjs} from 'react-pdf';
 
 export default function Test() {
 
     pdfjs.GlobalWorkerOptions.workerSrc =
         `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
@@ -28,7 +21,7 @@ export default function Test() {
     });
 
     /*When document gets loaded successfully*/
-    function onDocumentLoadSuccess({ numPages }) {
+    function onDocumentLoadSuccess({numPages}) {
         setNumPages(numPages);
         setPageNumber(1);
     }
@@ -44,97 +37,56 @@ export default function Test() {
     function nextPage() {
         changePage(1);
     }
-    function fileLoad() {
-        /*
-                setFile("https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK")
-        */
-                /*fetch('http://localhost:3005/a')*/
-                /*    .then(res=>res.text())
-                    .then(data=>{console.log("data", data); setFile(data)})
-*/
-        fetch('http://localhost:3005/a')
-            .then((data)=>{
-/*
-                var url = window.URL.createObjectURL(data),
-*/
-                var url = window.URL.createObjectURL(new Blob([data])),
-                    anchor = document.createElement("a");
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute(
-                    'download',
-                    `FileName.pdf`,
-                );
-                console.log("link", link)
-                link.click();
-            })
-
-      /*  var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';*/
 
 
-    /*    fetch('http://localhost:3005/a', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-        }).then(res => res.text())
-            .then(data => {
-                console.log("data", data);
-                setFile(data)
-            })*/
-    }
-    return (
-        <>
-            <label htmlFor="file">
-                Load from file: <input onChange={onFileChange} type="file" />
-            </label>
-            <label>
-                INPUT TO HHTPS <input onChange={(e)=>{setFile(e.target.value[0]); setTimeout(()=>console.log("e.target.value[0]", e.target.value),0)}}/>
-            </label>
-            <div className="main">
-                <button onClick={fileLoad}>Przycisk</button>
-                <div>
-                    <div className="pagec">
-                        Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+
+        return (
+            <>
+                <label htmlFor="file">
+                    Load from file: <input onChange={onFileChange} type="file"/>
+                </label>
+                <label>
+                    INPUT TO HHTPS <input onChange={(e) => {
+                    setFile(e.target.value[0]);
+                    setTimeout(() => console.log("e.target.value[0]", e.target.value), 0)
+                }}/>
+                </label>
+                <div className="main">
+
+{/*
+                    <button onClick={fileLoad}>Przycisk</button>
+*/}
+
+                    <div>
+                        <div className="pagec">
+                            Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+                        </div>
+                        <div className="buttonc">
+                            <button
+                                type="button"
+                                disabled={pageNumber <= 1}
+                                onClick={previousPage}
+                                className="Pre"
+
+                            >
+                                Previous
+                            </button>
+                            <button
+                                type="button"
+                                disabled={pageNumber >= numPages}
+                                onClick={nextPage}
+                            >
+                                Next
+                            </button>
+                        </div>
                     </div>
-                    <div className="buttonc">
-                        <button
-                            type="button"
-                            disabled={pageNumber <= 1}
-                            onClick={previousPage}
-                            className="Pre"
-
-                        >
-                            Previous
-                        </button>
-                        <button
-                            type="button"
-                            disabled={pageNumber >= numPages}
-                            onClick={nextPage}
-
-                        >
-                            Next
-                        </button>
-                    </div>
+                    <Document
+                        file={"http://localhost:3005/a"}
+                        onLoadSuccess={onDocumentLoadSuccess}
+                    >
+                        <Page pageNumber={pageNumber}/>
+                    </Document>
                 </div>
-{/*                <div> DOC FROM    ->>>   https:/cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK </div>
-                <Document
-                    file={"https://cdn.filestackcontent.com/wcrjf9qPTCKXV3hMXDwK"}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={pageNumber} />
-                </Document>
-           <div> DOC FROM SETFILE </div>
-
-                */}
-
-                <Document
-                                        file={file}
-                    onLoadSuccess={onDocumentLoadSuccess}
-                >
-                    <Page pageNumber={pageNumber} />
-                </Document>
-            </div>
-        </>
-    );
-}
+            </>
+        );
+   }
